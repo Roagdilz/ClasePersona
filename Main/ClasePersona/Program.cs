921dbebca36cdc1adf520c423db647b08031b6a0
Roagdilz <diazlopezrodrigo14@gmail.com>
2023-04-17 17:44:05 -0300

    New

---
+++ Main/ClasePersona/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Rodrigo
+ * Date: 17/4/2023
+ * Time: 17:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ClasePersona
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
